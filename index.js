343ba96c442c3799956bf9cbb2fb4c40d92f5430
yvette-luong <63530290+yvette-luong@users.noreply.github.com>
2020-11-13 11:54:42 -0800

    server up

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const server = require('./api/server.js');
 
 const PORT = process.env.PORT || 3300;
 server.listen(PORT, () => {
-  console.log(`\n=== Server listening on port ${PORT} ===\n`);
+  console.log(`\n=== Server listening on port ${PORT} === http://localhost:${PORT}\n`);
 });
